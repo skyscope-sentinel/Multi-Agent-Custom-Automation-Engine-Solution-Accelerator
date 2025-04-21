b0c9ca6ae061f13cbf58123d6642ba4c74ec9113
DB Lee <donlee@microsoft.com>
2025-04-21 22:40:35 +0000

    add uv dependency manager and update devcontainer (<- test codespace)

--- .devcontainer/setupEnv.sh
+++ .devcontainer/setupEnv.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
-pip install --upgrade pip
+# pip install --upgrade pip
+echo "Setting up Python environment in backend"
+cd ./src/backend
+uv sync --frozen
+
+cd ../../
+
+echo "Setting up Python environment in frontend"
+cd ./src/frontend
+uv sync --frozen
+
+cd ../../
+
+echo "dependency setup completed."
+
+# (cd ./src/frontend; pip install -r requirements.txt)
 
 
-(cd ./src/frontend; pip install -r requirements.txt)
-
-
-(cd ./src/backend; pip install -r requirements.txt)
+# (cd ./src/backend; pip install -r requirements.txt)
 
 
